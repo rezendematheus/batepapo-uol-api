692f18e859ae685e8f6ef6c7b69c4759682c9ab1
Matheus Rezende <matheusreisrezende.2002@gmail.com>
2023-01-14 21:25:56 -0300

    participants get, messages post

--- src/app.js
+++ src/app.js
@@ -23,36 +23,84 @@ const timeNow = dayjs().format('HH:mm:ss');
 /* Participants Routes */
 
 app.post('/participants', async (req, res) => {
-    const { name } = req.body
-
-    const participantsSchema = joi.object({
-        name: joi.string().required()
-    })
-
-    const validation = participantsSchema.validate(name, { aboutEarly: true })
-    if(validation.error){
-        const errors = validation.error.details.map(detail => {
-            detail.message
-        })
-        res.status(422).send(errors)
-    }
-
     try{
+        const { name } = req.body;
+        
+        const nameExists = await db.collection('participants').findOne({name: name})
+        if(nameExists){
+            return res.status(409)
+        }
+
+        const participantsSchema = joi.object({
+            name: joi.string().required()
+        })
+        
+        const validation = participantsSchema.validate({name: name}, { abortEarly: true })
+
+        if(validation.error){
+            const errors = validation.error.details.map(detail => {detail.message })
+            res
+                .status(422)
+                .send(errors)
+            mongoClient.close()
+        }
+        
         await db.collection('participants').insertOne({name: name, lastStatus: Date.now()})
+
         await db.collection('messages').insertOne({from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: timeNow})
-        res.sendStatus(201)
+
+        res.status(201)
     } catch(err){
-        return res.sendStatus(500).send("algo deu errado")
+        return res.status(500)
     }
 })
 
 app.get('/participants', async (req, res) => {
+    try{
+        const participants = await db.collection('participants').find().toArray()
 
+        res.send(participants)
+    } catch(err){
+        return res.sendStatus(500).send(err.message)
+    }
 })
 
 /* Messages Routes */
 
 app.post('/messages', async (req, res) => {
+    const {to, text, type} = req.body
+    const { from } = req.header
+    try{
+        const senderExists = await db.collection('participants').find({name: from})
+        if(!senderExists){
+            return res.status(422)
+        }
+        const messageSchema = joi.object({
+            to: joi.string().required(),
+            text: joi.string().required(),
+            type: joi.string().allow('message', 'private_message').required()
+        })
+
+        const validation = messageSchema
+            .validate({
+                to: to,
+                text: text,
+                type: type
+            }, {abortEarly: false})
+
+        if(validation.error) {
+            const errors = validation.error.details.map(detail => detail.message)
+            res
+                .status(422)
+                .send(errors)
+            mongoClient.close()
+        }
+
+        await db.collection('messages').insertOne({from: from, to: to, text: text, type: type, time: timeNow})
+        res.status(201)
+    }catch(err){
+        return res.status(500)
+    }
 
 })
 
